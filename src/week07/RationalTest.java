0ea242a3f1ed85f9aad32ce53e9d0d3e423a20c7
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2018-03-20 09:41:02 +0300

    Rational class.

---
+++ src/week07/RationalTest.java
@@ -0,0 +1,19 @@
+package week07;
+
+/**
+ * This class tests the Rational class by performing
+ * rational number operations.
+ *
+ * @author: Eric Roberts
+ */
+
+public class RationalTest {
+    public static void main(String[] args) {
+        Rational a = new Rational(1, 2);
+        Rational b = new Rational(1, 3);
+        Rational c = new Rational(1, 6);
+        Rational sum1 = a.add(b);
+        Rational sum = sum1.add(c);
+        System.out.println(a + " + " + b + " + " + c + " = " + sum);
+    }
+}
---
+++ src/week07/RationalTest2.java
@@ -0,0 +1,18 @@
+package week07;
+
+/**
+ * This class tests the Rational class by performing
+ * rational number operations.
+ *
+ * @author: Eric Roberts
+ */
+
+public class RationalTest2 {
+    public static void main(String[] args) {
+        Rational a = new Rational(); // 0
+        Rational b = new Rational(3); // 3/1
+        Rational c = new Rational(2); // 2/1
+        Rational div = a.subtract(b).divide(c);
+        System.out.println("(" + a + "-" + b + ")/" + c + " = " + div);
+    }
+}
